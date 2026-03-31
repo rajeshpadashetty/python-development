6bcc1f9652acb54013277140a685b38264dcd34a
rajeshpadashetty <167882296+rajeshpadashetty@users.noreply.github.com>
2026-03-31 11:00:41 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from .write_file import write_data
-from .read_file import read_data
-from .append_file import append_data
-from .delete_file import delete_file
+from calculator import add ,sub,mul,div
+
+print(add.add(2,3))
+print(sub.sub(10,5))
+print(mul.mul(4,5))
+print(div.div(10,2))
