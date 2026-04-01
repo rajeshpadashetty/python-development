d6eee60c5052652b5ec068db3664c5c0916e6218
rajeshpadashetty <167882296+rajeshpadashetty@users.noreply.github.com>
2026-04-01 22:23:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-from schedule.add_task import add_task
-from schedule.view_tasks import view_tasks
-from schedule.run_tasks import run_tasks
+import pandas as pd
 
-def menu():
-    while True:
-        print("\n1. Add Task")
-        print("2. View Tasks")
-        print("3. Run Scheduler")
-        print("4. Exit")
-
-        choice = input("Enter choice: ").strip()
-
-        if choice == "1":
-            name = input("Enter task name: ")
-            time = input("Enter time (HH:MM): ")
-            add_task(name, time)
-
-        elif choice == "2":
-            view_tasks()
-
-        elif choice == "3":
-            run_tasks()
-
-        elif choice == "4":
-            break
-
-        else:
-            print("Invalid choice")
-
-menu()
+df = pd.read_csv("data.csv",index_col=Name)
+print(df.to_string(index=False))
+print(df.loc("name"))
