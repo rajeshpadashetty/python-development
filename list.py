cc3d83c71ef570df836b8e1f808b1c2bcbed967a
rajeshpadashetty <167882296+rajeshpadashetty@users.noreply.github.com>
2026-04-04 16:13:46 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+my_list=[1,2,3,4,5,6,7,8,9]
+print(my_list)
---
+++ shape.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+array = np.array([
+    [['A','B','C'], [1,2,3], ['a','b','c']],
+    [['E','F','G'], [5,6,7], ['e','f','g']],
+    [['I','J','K'], [9,10,11], ['i','j','k']]
+])
+print(array.shape)
